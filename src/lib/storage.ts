814a64ece1fe2655af520fa9aa2baeda0043c291
tentwinkle <173590892+tentwinkle@users.noreply.github.com>
2025-04-05 01:31:14 -0700

    Update storage.ts

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -39,7 +39,7 @@ export const storage = {
       
       const { data: uploadData, error } = await supabaseClient
         .storage
-        .from('files')
+        .from('uploads')
         .upload(`public/${filename}`, data, {
           contentType,
           upsert: false
@@ -70,7 +70,7 @@ export const storage = {
       
       const { data, error } = await supabaseClient
         .storage
-        .from('files')
+        .from('uploads')
         .download(filePath);
       
       if (error) throw error;
